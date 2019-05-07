47f0a00a689f843cb2b0aa74d8e6f48feaf57b75
brandolern <46857695+brandolern@users.noreply.github.com>
2019-05-06 18:52:51 -0600

    finished routing

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [{
+    "name": "Joey",
+    "picture": "example.jpg",
+    "scores": [
+        5,
+        2,
+        3,
+        5,
+        5,
+        2,
+        1,
+    ]
+}];
+module.exports = friends;
