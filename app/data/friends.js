0a88820f986c8e6bf876cf57e7c16690c6f7457e
brandolern <46857695+brandolern@users.noreply.github.com>
2019-05-10 16:32:05 -0600

    added friends to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,7 +16,7 @@ var friends = [{
     },
     {
         "name": "Joey Jorgensen",
-        "picture": "",
+        "picture": "https://s3-media3.fl.yelpcdn.com/buphoto/q5MUuLPMZNfuhmR17VyL_A/o.jpg",
         "scores": [
             1,
             5,
@@ -32,7 +32,7 @@ var friends = [{
     },
     {
         "name": "Hailey Hutchensen",
-        "picture": "example.jpg",
+        "picture": "https://northcentraluniversity.files.wordpress.com/2014/03/tammy-harosky-portrait.jpg",
         "scores": [
             3,
             2,
@@ -48,7 +48,7 @@ var friends = [{
     },
     {
         "name": "Tony Corleone",
-        "picture": "example.jpg",
+        "picture": "https://pbs.twimg.com/profile_images/581453156388765696/sY6-Diep_400x400.jpg",
         "scores": [
             1,
             4,
@@ -64,7 +64,7 @@ var friends = [{
     },
     {
         "name": "Brenda Baker",
-        "picture": "example.jpg",
+        "picture": "http://d21gd0ap5v1ndt.cloudfront.net/web02/img.php?src=mug_shots/2018-19/brenda_baker_2119_wb2.jpg&v8e95042d2f1fb236b454085cf1e8c090a37ce7e7&site=ocu&width=169&height=224&crop",
         "scores": [
             5,
             5,
@@ -80,7 +80,7 @@ var friends = [{
     },
     {
         "name": "Samuel L. Jackson",
-        "picture": "example.jpg",
+        "picture": "https://pixel.nymag.com/imgs/daily/vulture/2018/06/13/13-samuel-l-jackson.w700.h700.jpg",
         "scores": [
             1,
             1,
@@ -95,8 +95,8 @@ var friends = [{
         ]
     },
     {
-        "name": "",
-        "picture": "example.jpg",
+        "name": "The Hound",
+        "picture": "https://imgix.bustle.com/2016/4/5/sandor-c266399a-e387-49b7-bf3e-de1bbc8bb618.jpg",
         "scores": [
             5,
             2,
@@ -111,8 +111,8 @@ var friends = [{
         ]
     },
     {
-        "name": "",
-        "picture": "example.jpg",
+        "name": "Roger Waters",
+        "picture": "https://www.operademontreal.com/sites/default/files/styles/mobile/public/librettists/waters_roger.jpg?itok=Y7NP1LWx",
         "scores": [
             5,
             5,
@@ -127,35 +127,35 @@ var friends = [{
         ]
     },
     {
-        "name": "",
-        "picture": "example.jpg",
+        "name": "William Shakespeare",
+        "picture": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNzgzMzkwNzMz/william-shakespeare-194895-1-402.jpg",
         "scores": [
-            5,
-            2,
             3,
-            5,
-            5,
+            3,
+            3,
+            2,
             2,
             1,
             3,
-            1,
-            5
+            2,
+            4,
+            3
         ]
     },
     {
-        "name": "",
-        "picture": "example.jpg",
+        "name": "Blair Bertram",
+        "picture": "http://www.whatisgrace.org/assets/2479/9_adult_discipleshipblairlerner.png",
         "scores": [
             5,
-            2,
-            3,
             5,
             5,
-            2,
-            1,
-            3,
-            1,
-            5
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            4
         ]
     }
 ];
