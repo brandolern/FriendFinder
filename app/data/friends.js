4be93d57c4d7b0abc2eb942b6a77f27bf59a5d0d
brandolern <46857695+brandolern@users.noreply.github.com>
2019-05-10 16:07:36 -0600

    fixed middleware issue

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,162 @@
 var friends = [{
-    "name": "Joey",
-    "picture": "example.jpg",
-    "scores": [
-        5,
-        2,
-        3,
-        5,
-        5,
-        2,
-        1,
-    ]
-}];
+        "name": "Bob Blart",
+        "picture": "https://echosrecordbar.co.za/storage/76981/conversions/312jxn8pUhINNnxQSujnTUlwlwT-medium.jpg",
+        "scores": [
+            4,
+            1,
+            3,
+            3,
+            1,
+            5,
+            5,
+            1,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Joey Jorgensen",
+        "picture": "",
+        "scores": [
+            1,
+            5,
+            2,
+            3,
+            3,
+            3,
+            5,
+            4,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Hailey Hutchensen",
+        "picture": "example.jpg",
+        "scores": [
+            3,
+            2,
+            3,
+            3,
+            2,
+            3,
+            2,
+            3,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Tony Corleone",
+        "picture": "example.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            3,
+            4,
+            2,
+            3,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Brenda Baker",
+        "picture": "example.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            4,
+            3,
+            5,
+            4,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Samuel L. Jackson",
+        "picture": "example.jpg",
+        "scores": [
+            1,
+            1,
+            2,
+            2,
+            3,
+            5,
+            1,
+            1,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "",
+        "picture": "example.jpg",
+        "scores": [
+            5,
+            2,
+            3,
+            5,
+            5,
+            2,
+            1,
+            3,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "",
+        "picture": "example.jpg",
+        "scores": [
+            5,
+            5,
+            1,
+            1,
+            3,
+            3,
+            5,
+            4,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "",
+        "picture": "example.jpg",
+        "scores": [
+            5,
+            2,
+            3,
+            5,
+            5,
+            2,
+            1,
+            3,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "",
+        "picture": "example.jpg",
+        "scores": [
+            5,
+            2,
+            3,
+            5,
+            5,
+            2,
+            1,
+            3,
+            1,
+            5
+        ]
+    }
+];
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,9 +3,24 @@ var friendData = require("../data/friends");
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendData);
-        console.log("got it");
     });
     app.post("/api/friends", function (req, res) {
+        req.body.scores = req.body.scores.map(x => parseFloat(x));
+        var currentUser = req.body.scores;
+        var totalDifferenceArr = [];
+        var counter = 0;
+
+        while (counter < friendData.length) {
+            var totalDifference = 0;
+            for (i = 0; i < friendData.length; i++) {
+                totalDifference += Math.abs(friendData[counter].scores[i] - currentUser[i]);
+            };
+            totalDifferenceArr.push(totalDifference);
+            counter++;
+        };
+        var match = friendData[totalDifferenceArr.indexOf(Math.min(...totalDifferenceArr))];
+
         friendData.push(req.body);
+        res.json(match);
     });
 };
